17455b891344c124cfe4662334e2420d51fcdf6a
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-05-12 19:10:53 -0300

    search github issues

---
+++ src/chapter_4/github.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+const IssuesURL = "https://api.github.com/search/issues"
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
+}
+
+type Issue struct {
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string
+}
+
+type IssuesSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	query := url.QueryEscape(strings.Join(terms, " "))
+
+	response, err := http.Get(IssuesURL + "?q=" + query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("search query failed: %s", response.Status)
+	}
+
+	var result IssuesSearchResult
+	err = json.NewDecoder(response.Body).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+func main() {
+	result, err := SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d issues:\n", result.TotalCount)
+
+	for _, item := range result.Items {
+		fmt.Printf("#%-d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+}
